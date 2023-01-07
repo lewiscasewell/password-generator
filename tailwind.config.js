bcba738bbec6f4228824a9c92c2aa64f88eada54
lewiscasewell <email>
2023-01-07 18:30:54 +0000

    clean up of styling, added a guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient( circle at 50% 50%,var(--background) 20%,transparent 20.5%,transparent 49.5%,var(--background) 50%)",
+      },
+    },
   },
   plugins: [],
 };
---
+++ utils/use-follow-pointer.ts
@@ -0,0 +1,27 @@
+import { useScroll } from "framer-motion";
+import { useState, RefObject, useEffect } from "react";
+
+export function useFollowPointer(ref: RefObject<HTMLElement>) {
+  const [point, setPoint] = useState({ x: 0, y: 0 });
+  const { scrollY } = useScroll();
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const handlePointerMove = ({ clientX, clientY }: MouseEvent) => {
+      const element = ref.current!;
+
+      const x = clientX - element.offsetLeft - element.offsetWidth / 2;
+      const y =
+        clientY - element.offsetTop - element.offsetHeight / 2 + scrollY.get();
+
+      setPoint({ x, y });
+    };
+
+    window.addEventListener("pointermove", handlePointerMove);
+
+    return () => window.removeEventListener("pointermove", handlePointerMove);
+  }, [ref, scrollY]);
+
+  return point;
+}
